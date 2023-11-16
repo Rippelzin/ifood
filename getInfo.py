a32de1c69e5bb0c65fdecda2a605dd55c5fd5e2b
Gabriel Rippel Lima <gabrielrippelima@gmail.com>
2023-11-16 16:40:12 -0300

    getinfo arrumado

--- getInfo.py
+++ getInfo.py
@@ -3,68 +3,82 @@ from selenium import webdriver
 from bs4 import BeautifulSoup
 import time
 
-
-
-url = 'https://script.google.com/macros/s/AKfycbyinhEEiW_ZkxjT5SRRsMsfRoCmWJMXQxI6gjBnejwwus7mgfLFEfk7HBcr6rX1Bg2Zvg/exec'
-
-response = requests.get(url)
-cnpj = response.json()
-
+from collections import defaultdict
 
 options = webdriver.ChromeOptions()
 options.add_experimental_option("detach", True)
-browser = webdriver.Chrome(options=options)
-
-#contato, socios, capital social, cnae, data de abertura, razao social e localizacao
-
-browser.get('https://cnpj.biz/'+cnpj)
-
-page_content = browser.page_source
-
-site = BeautifulSoup(page_content, 'html.parser')
 
 
-copys = site.find_all('b', attrs={'class': 'copy'})
-p = site.find_all('p')
 
-#for copy in copys:
-#    print(copy.text)
+b = range(6666)
+for a in b:
+    url = 'https://script.google.com/macros/s/AKfycbzR2TqHJMaD61h8_16VZxIqQYsrdKfcWXWLn_tqm7dBUvSbLNvRWoSYITkNFfYlvC3sHA/exec'
 
-cep = ''
-razao = ''
-data = ''
-capitalSocial = ''
-contato = ''
-email=''
-socio = []
+    response = requests.get(url)
+    cnpj = response.json()
 
-for x in p:
-    y = x.text
-    if 'CEP' in y :
-        cep = y[4:14]
-    if 'Razão Social' in y :
-        razao = y[13:-19]
-    if 'Data da Abertura' in y :
-        data = y[17:28]
-    if 'Capital Social' in y :
-        capitalSocial = y[16:-21]
-    if 'E-mail' in y:
-        email = y[8:-36]
-    if 'Telefone' in y :
-        contato = y[14:29]
-    if 'Sócio' in y:
-        socio.append(y)
+    browser = webdriver.Chrome(options=options)
 
-divs = site.find_all('div', attrs={'class': 'col c12'})
-cnae = []
-for div in divs:
-    print(div.text)
-    if 'Principal' in div:
-        cnae.append(div.text)
+    browser.get('https://cnpj.biz/'+cnpj)
+
+    page_content = browser.page_source
+
+    site = BeautifulSoup(page_content, 'html.parser')
 
 
+    copys = site.find_all('b', attrs={'class': 'copy'})
+    p = site.find_all('p')
+
+    #for copy in copys:
+    #    print(copy.text)
+
+   
+
+    objeto = {
+        'cnpj': '',
+        'cep' : '',
+        'razao' : '',
+        'data': '',
+        'capitalSocial': '',
+        'contato' :'',
+        'email': '',
+        'socio': [],
+        'cnae': [],
+        }
+
+    objeto = defaultdict(list)
+
+    for x in p:
+        y = x.text
+        if 'CEP' in y :
+            objeto['cep'] = y[4:14]
+        if 'Razão Social' in y :
+            objeto['razao'] = y[13:-19]
+        if 'Data da Abertura' in y :
+            objeto['data'] = y[17:28]
+        if 'Capital Social' in y :
+            objeto['capitalSocial'] = y[16:-21]
+        if 'E-mail' in y:
+            objeto['email'] = y[8:-36]
+        if 'Telefone' in y :
+            objeto['contato'] = y[14:29]
+        if 'Sócio' in y:
+            objeto['socio'].append(y)
+  
     
+    u = site.find_all('u')
+    for l in u:
+        print(l.text)
+        objeto['cnae'].append(l.text)
+
+    objeto['cnpj'] = cnpj
+    print(objeto)
+
+    urlPost = 'https://script.google.com/macros/s/AKfycbxyBD73GSgnqkrvFlClSDrSiTA_lmyaEsEEPpmzNdg88k2uUQWX5Z3GY9IJ4FCIkPcaCQ/exec'
+    response = requests.post(urlPost, data=objeto)
     
+    browser.close()
+"""
 print(cep)  
 print(razao)
 print(data)
@@ -74,7 +88,7 @@ print(email)
 print(socio[0])
 print(cnae)
 
-
+"""
 
 """
 data = {
